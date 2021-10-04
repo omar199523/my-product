3bc362fd83e990a2c26726842882ccdacb4dc2e5
omar199523 <model199523@github.com>
2021-10-04 10:11:39 +0300

    add some style
    edit and use typograhp in main bocument  section

--- src/componants/DocumentSection/index.js
+++ src/componants/DocumentSection/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import documentLogo from '../../img/documentLogo.svg'
+import documentLogo from '../../img/documentLogo.svg';
+import {H2,P3,P2} from '../Typograph'
 import './style.css'
  const DocumentSection = () => {
     return (
         <section className="container docu-cont">
-            <div className ="docu-img"><img src={documentLogo}/></div>
+            <div className ="docu-img"><img src={documentLogo} alt=""/></div>
             <div className="docu-text">
-                <h4>Effortless Validation for</h4>
-                <h2>Management</h2>
-                <p>The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person. </p>
-                <h3>Accessory makers</h3>
-                <p>While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun</p>
-                <h3>Alterationists</h3>
-                <p>If you are looking for a new way to promote your business that won’t cost you more money,</p>
+                <P2>Effortless Validation for</P2>
+                <H2>Management</H2>
+                <P3>The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person. </P3>
+                <P2>Accessory makers</P2>
+                <P3>While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun</P3>
+                <P2>Alterationists</P2>
+                <P3>If you are looking for a new way to promote your business that won’t cost you more money,</P3>
             </div>
         </section>
     )
