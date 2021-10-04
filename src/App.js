c9ebae8557ead7c18038ecdf188596f1f27e646d
omar199523 <model19952323@gmail.com>
2021-10-04 17:51:48 +0300

    edit app.js file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './componants/Header';
 import MainLogo from './componants/MainLogo';
-import DocumentSection from './componants/DocumentSection'
+import DocumentSection from './componants/DocumentSection';
+import ServiceSection from './componants/ServicesSection';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MainLogo/>
+      <ServiceSection/>
       <DocumentSection/>
     </div>
   );
