3fde488111c1708392689a2038156a3af013ed56
omar199523 <model199523@github.com>
2021-10-04 12:25:38 +0300

    edit some general style and componant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './componants/Header';
 import MainLogo from './componants/MainLogo';
+import DocumentSection from './componants/DocumentSection';
+import CustomarSuport from './componants/CustomarSuport';
 import './App.css'
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <MainLogo/>
+      <Header/> 
+       <MainLogo/>
+      <DocumentSection/>
+      <CustomarSuport/>
     </div>
   );
 }
