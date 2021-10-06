80459fa2d9955ac545b3488145d0024f50e0219d
omar199523 <model19952323@gmail.com>
2021-10-06 17:49:07 +0300

    add CollaborativeSection  in product page and edit some style in this section

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../componants/Header';
 import MainLogo from '../componants/MainLogo';
-import DocumentSection from '../componants/DocumentSection'
+
 // import Footer from '../componants/Footer';
 import ServiceSection from '../componants/ServicesSection';
+import DocumentSection from '../componants/DocumentSection'
+import CollaborativeSection from '../componants/CollaborativeSection'
 import CustomarSuport from '../componants/CustomarSuport'
 const Product = () => {
     return (
@@ -13,6 +15,7 @@ const Product = () => {
             <ServiceSection/>
             <DocumentSection/>
             <CustomarSuport/>
+            <CollaborativeSection/>
             {/* <Footer/> */}
         </>
     )
