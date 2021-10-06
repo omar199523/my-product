67b0fb47fcd0dca8e2a6d4fb18d276292b4a0d72
omar199523 <model199523@github.com>
2021-10-06 12:01:44 +0300

    some edit in app.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ function App() {
   return (
     <div className="App">
       <Product/>
-import Header from './componants/Header';
-import MainLogo from './componants/MainLogo';
-import DocumentSection from './componants/DocumentSection';
-import CustomarSuport from './componants/CustomarSuport';
-import './App.css'
-function App() {
-  return (
-    <div className="App">
-      <Header/> 
-       <MainLogo/>
-      <DocumentSection/>
-      <CustomarSuport/>
     </div>
   );
 }
