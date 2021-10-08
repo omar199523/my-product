71e728e911b5ae43450f075b83b1f22d99de25ea
omar199523 <model19952323@gmail.com>
2021-10-08 20:26:29 +0300

    edit some style in customar and typograph section

--- src/componants/JoinSection/index.js
+++ src/componants/JoinSection/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {H2} from '../Typograph'
-import '.style.css'
+import joinImgae from "../../img/joinImgae.svg"
+import './style.css'
 export const JoinSection = () => {
     return (
         <section className="container">
             <div>
                 <H2>Join 100 Compannies who boost their business with Product</H2>
                 <button>Get This</button>
+            </div>
+            <div>
+                <img src={joinImgae} alt=""/>
             </div>
             <div><img src="" alt= ""/></div>
         </section>
