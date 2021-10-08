a4741ff945fb3615219e35a3bf0696f82a9cb025
omar199523 <model19952323@gmail.com>
2021-10-08 20:24:07 +0300

    add pages structer

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import Header from '../componants/Header';
-import MainLogo from '../componants/MainLogo';
 
+import MainLogo from '../componants/MainLogo';
 // import Footer from '../componants/Footer';
 import ServiceSection from '../componants/ServicesSection';
 import DocumentSection from '../componants/DocumentSection'
 import CollaborativeSection from '../componants/CollaborativeSection'
 import CustomarSuport from '../componants/CustomarSuport'
 import QuickSection from '../componants/QuickSection'
+import ContentsSection from '../componants/ContentsSection'
+import JoinSection from '../componants/JoinSection'
 const Product = () => {
     return (
         <>
-            <Header/>
             <MainLogo/>
             <ServiceSection/>
             <DocumentSection/>
             <CustomarSuport/>
             <CollaborativeSection/>
             <QuickSection/>
+            <ContentsSection/>
+            <JoinSection/>
             {/* <Footer/> */}
         </>
     )
