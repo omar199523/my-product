1cfa2ed03e333c8b078096247c4855c51d02f0f9
omar199523 <model199523@github.com>
2021-10-09 13:09:31 +0300

    add content section

--- src/componants/ContentsSection/index.js
+++ src/componants/ContentsSection/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {H2,H3,P1,P3} from '../Typograph'
+import {H2,H3,P3} from '../Typograph';
+import content1 from '../../img/content1.svg';
+import content2 from '../../img/content2.svg';
+import content3 from '../../img/content3.svg';
 import './style.css';
 
  const ContentsSection = () => {
@@ -9,18 +12,18 @@ import './style.css';
             <P3 className="contents-parg">We focus on ergonomics and meeting you where you work. It's only a keystroke away. </P3>
             <div>
                 <div className="col-3 contnet-col-1">
-                    <img src="{}" alt=""/>
-                    <P3>By <P1>Wahid Ari </P1>|  03 March 2019</P3>
+                    <img src={content1} alt=""/>
+                    <P3>By <span>Wahid Ari </span>|  03 March 2019</P3>
                     <H3>Increasing Prosperity With Positive Thinking</H3>
                 </div>
                 <div className="col-3 contnet-col-2">
-                    <img src="{}" alt=""/>
-                    <P3>By <P1>Wahid Ari </P1>|  03 March 2019</P3>
+                    <img src={content2} alt=""/>
+                    <P3>By <span>Wahid Ari </span>|  03 March 2019</P3>
                     <H3>Motivation Is The First Step To Success</H3>
                 </div>
                 <div className="col-3 contnet-col-3">
-                    <img src="{}" alt=""/>
-                    <P3>By <P1>Wahid Ari </P1>|  03 March 2019</P3>
+                    <img src={content3} alt=""/>
+                    <P3>By <span>Wahid Ari </span>|  03 March 2019</P3>
                     <H3>Success Steps For Your Personal Or Business</H3>
                 </div>
                 <div className="smoll-circle">
