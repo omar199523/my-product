61941bb49db87d715ee4f7ec014e8b1624898ced
omar199523 <model199523@github.com>
2021-10-18 09:23:58 +0300

    create main footer

--- src/componants/Footer/index.js
+++ src/componants/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {H1,H3,H6,P3} from '../Typograph';
+import {H1,H4,P3} from '../Typograph';
 import { Link } from 'react-router-dom';
 import logo from '../../img/logo.svg';
 import instgam from '../../img/icon/instgram.svg';
@@ -21,25 +21,25 @@ const Footer = () => {
                 </div>
             </div>
             <ul className="col-4">
-                <H3>Resource</H3>
+                <H4>Resource</H4>
                 <Link to=""><li>About Us</li></Link>
                 <Link to=""><li>Blog</li></Link>
                 <Link to=""><li>Contact</li></Link>
                 <Link to=""><li>FAQ</li></Link>
             </ul>
             <ul className="col-4">
-                <H3>Legal Stuff</H3>
+                <H4>Legal Stuff</H4>
                 <Link to=""><li>Disclaimer</li></Link>
                 <Link to=""><li>Financing</li></Link>
                 <Link to=""><li>Privacy Policy</li></Link>
                 <Link to=""><li>Terms of Service</li></Link>
             </ul>
-            <div className="col-4">
-                <H3>knowing you're always on the best energy deal. </H3>
+            <div className="col-4 input-cont">
+                <H4>knowing you're always on the best energy deal. </H4>
                 <input type="text"/>
                 <button>Sign up Now</button>
             </div>
-            <P3 className="work-by">Made With Love By Omar Al.gamri </P3>
+            <P3 className="work-by">@ Made With Love By Omar Al.gamri </P3>
         </footer>
     )
 }
